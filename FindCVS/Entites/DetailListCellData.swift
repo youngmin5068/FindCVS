bfcdaff4b215df9a81bbb42dc59fb559ae6c29db
youngmin5068 <yougmin5068@naver.com>
2022-01-10 15:18:19 +0900

    Data를 받아올 Entites 구현

---
+++ FindCVS/Entites/DetailListCellData.swift
@@ -0,0 +1,16 @@
+//
+//  DetailListCellData.swift
+//  FindCVS
+//
+//  Created by 김영민 on 2022/01/10.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+    
+}
---
+++ FindCVS/Entites/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  FindCVS
+//
+//  Created by 김영민 on 2022/01/10.
+//
+
+import Foundation
+
+struct LocationData : Decodable {
+    let documents: [KLDocument]
+}
